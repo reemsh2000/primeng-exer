8cde40d2399435a85b471c4f7c8ddcf64e3a23e8
reemsh2000 <sh951652@gmail.com>
2022-11-06 17:25:22 +0200

    update import style file

--- src/app/components/general/general.component.ts
+++ src/app/components/general/general.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-general',
   templateUrl: './general.component.html',
-  styleUrls: ['./general.component.css']
+  styleUrls: ['./general.component.scss']
 })
 export class GeneralComponent implements OnInit {
 
--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.css'],
+  styleUrls: ['./user-details.component.scss'],
 })
 export class UserDetailsComponent {
   checked: boolean = false;
