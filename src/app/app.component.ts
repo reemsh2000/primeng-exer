f8745a1ec9b9012382e16f47123f6a3bf3791c52
reemsh2000 <sh951652@gmail.com>
2022-11-06 20:40:29 +0200

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'prime';
+  index: number = 0;
+  changeTab(indexTab:number) {
+    console.log("clicked")
+    console.log(indexTab)
+    this.index = indexTab;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,41 @@ import { CheckboxModule } from 'primeng/checkbox';
 import { FormsModule } from '@angular/forms';
 import { TabViewModule } from 'primeng/tabview';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { GeneralComponent } from './components/general/general.component';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { BasicComponent } from './components/basic/basic.component';
+import { ProjectInfoComponent } from './components/project-info/project-info.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     GeneralComponent,
     UserDetailsComponent,
+    BasicComponent,
+    ProjectInfoComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
     CheckboxModule,
     FormsModule,
     TabViewModule,
     ButtonModule,
-    TabViewModule
+    TabViewModule,
+    InputTextModule,
+    AutoCompleteModule,
+    CalendarModule,
+    DropdownModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
