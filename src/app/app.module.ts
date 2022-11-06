b10113b0f75ef1fac01e2fb5aca5c14ff59fdab8
reemsh2000 <sh951652@gmail.com>
2022-11-06 11:58:30 +0200

    intialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
+import { TabViewModule } from 'primeng/tabview';
+import { ButtonModule } from 'primeng/button';
 
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { GeneralComponent } from './components/general/general.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GeneralComponent,
+    UserDetailsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CheckboxModule,
+    FormsModule,
+    TabViewModule,
+    ButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
