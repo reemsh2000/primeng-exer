78765da4e9216f0b81f859a2c9f7d134e0466e82
reemsh2000 <sh951652@gmail.com>
2022-11-07 08:41:08 +0200

    put the data on file

---
+++ src/assets/data.ts
@@ -0,0 +1,9 @@
+
+
+export const titles = [
+  'Project Engineer - Civil',
+  'Project Engineer - E/M',
+  'Software Engineer',
+];
+export const orgunits = ['M01031', 'M01032', 'M01032', 'M01033'];
+export const calendars = ['FCW 8 Plus 1', 'FCW 8 Plus 2', 'FCW 9', 'FCW 10'];
